f195dbe329a8e25a3662ff94695b2528da7fc616
minhducle291 <minhducle291@gmail.com>
2025-04-13 04:27:58 +0700

    Update page

--- pages/báo cáo 1.py
+++ pages/báo cáo 1.py
@@ -1,12 +1,14 @@
 import streamlit as st
-from PIL import Image
-import time
 from sidebar import show_sidebar
-from utils import hide_default_sidebar
+from utils import hide_default_sidebar, apply_custom_styles
 
-st.set_page_config(page_title="Team mua chia Bách hóa XANH", layout="centered", initial_sidebar_state="auto", page_icon="assets/logo.png")
+# Cấu hình trang Streamlit
+st.set_page_config(page_title="Team mua chia Bách hóa XANH", layout="wide", initial_sidebar_state="auto", page_icon="assets/logo.png")
+apply_custom_styles()
 hide_default_sidebar()
 
+# Hiển thị sidebar khi đã đăng nhập
 show_sidebar()
-st.title("Trang chủ")
-st.success("Chào mừng bạn đã đăng nhập thành công!")
+
+# Tiêu đề trang
+st.title("Dashboard tổng hợp Fresh")
