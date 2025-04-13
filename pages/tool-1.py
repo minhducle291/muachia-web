62b7e892e212f285aa1424f410440a53408cd6dd
minhducle291 <minhducle291@gmail.com>
2025-04-14 01:50:20 +0700

    fix lỗi thư viện

--- pages/tool-1.py
+++ pages/tool-1.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sidebar import show_sidebar
 from utils import hide_default_sidebar, apply_custom_styles
 from io import BytesIO
+import openpyxl
 
 # Cấu hình trang Streamlit
 st.set_page_config(page_title="Team mua chia Bách hóa XANH", layout="wide", initial_sidebar_state="auto", page_icon="assets/logo.png")
